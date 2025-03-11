b2b214918ee7675259882b068b3645685da177b4
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-10 22:14:40 -0500

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,11 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>March Madness Predictor</h1>
+      <Link href="/matchup">Go to Matchup Predictor</Link>
+    </div>
+  );
+}
 
